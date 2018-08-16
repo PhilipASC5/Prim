aaef2743ba456ab2edbb76b5e52c5a23ce3796d2
PhilipASC5 <gflash51@gmail.com>
2018-08-16 19:52:40 -0400

    final

--- main.js
+++ main.js
@@ -2,7 +2,25 @@
 
 let i = 0;
 
-function onclick(){
+function submitButton(){
+    var comment = document.getElementById("comment").value;
+    localStorage.setItem(i,comment);
+    
+    i++;
 
-    localStorage.setItem(i++,document.getElementById("comment"));
+
+    let review = document.createElement("p");
+    review.innerText = comment;
+    const body = document.querySelector("body");
+    body.appendChild(review);
+    review.classList.add("review");
+console.log("hi");
+    let likeButton = document.createElement("div");
+    body.appendChild(likeButton)
 }
+
+
+
+
+
+
---
+++ rest.js
@@ -0,0 +1,8 @@
+function Restaurant(name, hours, menu, number, reviews){
+this.name = name;
+this.hours = hours;
+this.menu = menu;
+this.number = number;
+this.reviews = reviews;
+}
+var Restaurant = new Restaurant("Big Shot Bob's", "Monday-Thursday: 11:00AM-10:00PM Friday & Saturday: 11:00AM-11:00PM Sunday: NOON-9:00PM", "Ace Boogie, Lady Luck, Lebron to the Lakers", "412-343-7468")
